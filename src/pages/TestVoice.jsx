f9d411e0008a9b8857a420f18458bd0522d07035
ch-ayesha <108192526+ch-ayesha@users.noreply.github.com>
2025-04-29 00:24:58 +0500

    Update TestVoice.jsx

--- src/pages/TestVoice.jsx
+++ src/pages/TestVoice.jsx
@@ -44,7 +44,7 @@ function TestVoice() {
       
         try {
             
-          const response = await fetch("https://predictionparkinsons-kikcsmji2-ayesha-aslams-projects.vercel.app", {
+          const response = await fetch("https://predictionparkinsons-rk8v-6vt5jkbvh-ayesha-aslams-projects.vercel.app/", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
